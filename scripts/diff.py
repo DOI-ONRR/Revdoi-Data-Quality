c81c1b7b6c3770de8092579d60fd8c892bc54c99
echang97 <edward.chang@onrr.gov>
2019-08-05 10:27:57 -0400

    changed output

--- scripts/diff.py
+++ scripts/diff.py
@@ -44,14 +44,13 @@ def excel_diff(path_OLD, path_NEW):
             dfDiff = dfDiff.append(df_OLD.loc[row,:])
 
     dfDiff = dfDiff.sort_index().fillna('')
-    print('\nNew Rows:     {}'.format(newRows))
-    print('Dropped Rows: {}'.format(droppedRows))
+    output_string = '\nNew Rows:     {}'.format(newRows) + '\nDropped Rows: {}'.format(droppedRows)
     if len(changedCells) > 20:
         answer = input("There are a lot of changed cells. Print anyway? [y/n]")
         if (answer == 'y'):
-            print('Changed Cells: {}'.format(changedCells))
-    print('\nNew Columns:',newCols)
-    print('Dropped Columns:',droppedCols)
+            output_string += '\nChanged Cells: {}'.format(changedCells)
+    output_string += '\nNew Columns: {}'.format(newCols)
+    output_string += '\nDropped Columns: {}'.format(droppedCols)
 
     # Save output and format
     fname = '{} vs {}.xlsx'.format(path_OLD.stem,path_NEW.stem)
@@ -94,7 +93,7 @@ def excel_diff(path_OLD, path_NEW):
 
     # save
     writer.save()
-    print('\nDone. Exported DIFF to ' + str(Path.cwd()) + '\\output\\' + fname + '\n')
+    return output_string + '\nDone. Exported DIFF to ' + str(Path.cwd()) + '\\output\\' + fname + '\n'
 
 
 class Application(tk.Frame):
@@ -103,6 +102,7 @@ class Application(tk.Frame):
         super().__init__(master)
         self.old = StringVar()
         self.new = StringVar()
+        self.output = StringVar()
         self.padx = 5
         self.pady = 10
         self.entry_width = 94
@@ -111,32 +111,31 @@ class Application(tk.Frame):
 
     def create_widgets(self):
 
-        self.old_file = tk.Button(self)
-        self.old_file["text"] = "Select Old File"
-        self.old_file["command"] = self.set_old
-        self.old_file.grid(row=0, column=0, padx=self.padx, pady=self.pady)
+        old_file = tk.Button(self)
+        old_file["text"] = "Select Old File"
+        old_file["command"] = self.set_old
+        old_file.grid(row=0, column=0, padx=self.padx, pady=self.pady)
 
-        self.old_label = tk.Entry(self, textvariable=self.old)
-        self.old_label.config(width=self.entry_width)
-        self.old_label.grid(row=0, column=1)
+        old_label = tk.Entry(self, textvariable=self.old)
+        old_label.config(width=self.entry_width)
+        old_label.grid(row=0, column=1)
 
-        self.new_file = tk.Button(self)
-        self.new_file["text"] = "Select new File"
-        self.new_file["command"] = self.set_new
-        self.new_file.grid(row=1, column=0, padx=self.padx, pady=self.pady)
+        new_file = tk.Button(self)
+        new_file["text"] = "Select new File"
+        new_file["command"] = self.set_new
+        new_file.grid(row=1, column=0, padx=self.padx, pady=self.pady)
 
-        self.new_label = tk.Entry(self, textvariable=self.new)
-        self.new_label.config(width=self.entry_width)
-        self.new_label.grid(row=1, column=1)
+        new_label = tk.Entry(self, textvariable=self.new)
+        new_label.config(width=self.entry_width)
+        new_label.grid(row=1, column=1)
 
-        self.run_diff = tk.Button(self)
-        self.run_diff["text"] = "Run DIFF"
-        self.run_diff["command"] = self.start_diff
-        self.run_diff.grid(row=2, column=0, padx=self.padx, pady=self.pady)
-
+        run_diff = tk.Button(self)
+        run_diff["text"] = "Run DIFF"
+        run_diff["command"] = self.start_diff
+        run_diff.grid(row=2, column=0, padx=self.padx, pady=self.pady)
 
     def start_diff(self):
-        excel_diff(Path(self.old.get()), Path(self.new.get()))
+        print(excel_diff(Path(self.old.get()), Path(self.new.get())))
 
     def set_old(self):
         self.old.set(self.get_file())
